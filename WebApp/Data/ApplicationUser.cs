ef45ffb0f06596b1c1ebc7e098229b69c4d56057
bao0512 <baohcgcc200363@fpt.edu.vn>
2023-02-27 14:01:00 +0700

    Add project files.

---
+++ WebApp/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApp.Data
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? Acc_Name { get; set; }
+        public string? Acc_Image { get; set; }
+    }
+}
