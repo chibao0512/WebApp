70f30fa1e076f62c054b86d98c17e44eb88790db
bao0512 <baohcgcc200363@fpt.edu.vn>
2023-02-28 02:34:50 +0700

    Administrative authorization

---
+++ WebApp/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _db;
+        public AdminController(ApplicationDbContext db,
+            UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
