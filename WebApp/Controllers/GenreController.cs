5d82593241ce85178b343d507e83c937c01ef58f
mygcc20002 <ngodiemmy2211@gmail.com>
2023-02-28 15:46:21 +0700

    Controllers Book, Genre, Publisher

---
+++ WebApp/Controllers/GenreController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly ILogger<GenreController> _logger;
+
+        public GenreController(ILogger<GenreController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
