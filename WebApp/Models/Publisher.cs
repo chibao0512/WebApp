78d7e68c85d622694654410349752943b8917388
mygcc20002 <ngodiemmy2211@gmail.com>
2023-02-28 15:44:03 +0700

    model publisher

--- WebApp/Models/Publisher.cs
+++ WebApp/Models/Publisher.cs
@@ -1,6 +1,17 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Publisher
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Publisher_Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Publisher_Name { get; set; }
+        public string Publisher_Address { get; set; }
+        public virtual ICollection<Book>? books { get; set; }
+
     }
 }
