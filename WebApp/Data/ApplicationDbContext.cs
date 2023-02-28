62d57448e903340337693c3de1462c65313a7c5d
mygcc20002 <ngodiemmy2211@gmail.com>
2023-02-28 16:35:02 +0700

    fix bug application

--- WebApp/Data/ApplicationDbContext.cs
+++ WebApp/Data/ApplicationDbContext.cs
@@ -3,11 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WebApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }   
+        
+        public DbSet<Genre> genres {  get; set; }
+        public DbSet<Publisher> publishers { get; set; }
+        public DbSet<Book> books { get; set; }
     }
 }
