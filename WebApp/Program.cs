c3b524ddee52726780ab117572d8fc7bcbb06829
Nguyễn Công Thể <105303251+harynguyen@users.noreply.github.com>
2023-03-01 14:59:55 +0700

    Create session

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -17,8 +17,14 @@ builder.Services.AddDefaultIdentity<ApplicationUser>()
           .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
 
-
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(100000);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,7 +43,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthentication();
 app.UseAuthorization();
 
