79edd5db89a3694417ec91227db645b33bfe48b4
Nguyễn Công Thể <105303251+harynguyen@users.noreply.github.com>
2023-03-01 15:01:23 +0700

    Book controller for create cart

--- WebApp/Controllers/BookController.cs
+++ WebApp/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -12,13 +13,104 @@ namespace WebApp.Controllers
         {
             _db = db;
         }
+        public const string CARTKEY = "cart";
+        List<Cart> GetCartItems()
+        {
 
+            var session = HttpContext.Session;
+            string jsoncart = session.GetString(CARTKEY);
+            if (jsoncart != null)
+            {
+                return JsonConvert.DeserializeObject<List<Cart>>(jsoncart);
+            }
+            return new List<Cart>();
+        }
+        void ClearCart()
+        {
+            var session = HttpContext.Session;
+            session.Remove(CARTKEY);
+        }
+
+        // Lưu Cart (Danh sách CartItem) vào session
+        void SaveCartSession(List<Cart> ls)
+        {
+            var session = HttpContext.Session;
+            string jsoncart = JsonConvert.SerializeObject(ls);
+            session.SetString(CARTKEY, jsoncart);
+        }
         // index
         public IActionResult Index()
         {
             IEnumerable<Book> book = _db.books.Include(b => b.genre).ToList();
             return View(book);
         }
+        public IActionResult AddToCart(int id)
+        {
+            var book = _db.books.Where(p => p.Book_Id == id).FirstOrDefault();
+            if (book == null)
+                return NotFound("No found products");
+
+            var cart = GetCartItems();
+            var cartitem = cart.Find(p => p.book.Book_Id == id);
+            if (cartitem != null)
+            {
+                // Đã tồn tại, tăng thêm 1
+                cartitem.Cart_Quantity++;
+            }
+            else
+            {
+                //  Thêm mới
+                cart.Add(new Cart() { Cart_Quantity = 1, book = book });
+            }
+
+            // Lưu cart vào Session
+            SaveCartSession(cart);
+            // Chuyển đến trang hiện thị Cart
+            return RedirectToAction(nameof(Cart));
+        }
+        public IActionResult Cart()
+        {
+            return View(GetCartItems());
+        }
+        [Route("/removecart/{id:int}", Name = "removecart")]
+        public IActionResult RemoveCart(int id)
+        {
+            var cart = GetCartItems();
+            var cartitem = cart.Find(p => p.book.Book_Id == id);
+            if (cartitem != null)
+            {
+                // Đã tồn tại, tăng thêm 1
+                cart.Remove(cartitem);
+            }
+
+            SaveCartSession(cart);
+            return RedirectToAction(nameof(Cart));
+        }
+        [HttpPost]
+        [Route("/updatecart", Name = "updatecart")]
+        public IActionResult UpdateCart(int id, int quantities)
+        {
+            var cart = GetCartItems();
+            var cartitem = cart.Find(p => p.book.Book_Id == id);
+            if (cartitem != null)
+            {
+                // Đã tồn tại, tăng thêm 1
+                cartitem.Cart_Quantity = quantities;
+            }
+            SaveCartSession(cart);
+            // Trả về mã thành công (không có nội dung gì - chỉ để Ajax gọi)
+            return Ok();
+        }
+         public IActionResult CheckOut()
+        {
+            // Xử lý khi đặt hàng
+
+            return View();
+        }
+        /// <summary>
+        /// //////////////////////////////////////////////////////////////////////////////////////
+        /// </summary>
+
 
         // create
         public IActionResult Create()
